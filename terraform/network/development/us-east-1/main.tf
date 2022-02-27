4534e6d1937c925ce85afae07150910e4c3b1caf
Daniel Vincenzi <danielvincenzi@icloud.com>
2022-02-27 07:52:38 -0300

    Initial commit

--- terraform/network/development/us-east-1/main.tf
+++ terraform/network/development/us-east-1/main.tf
@@ -1,12 +1,21 @@
 locals {
-  region = "us-east-1"
+  eks_name = "dev-eks-20220223"
+  vpc_name = "dev-eks-vpc"
+  region   = "us-east-1"
+
+  tags = {
+    # Name        = local.eks_name
+    Environment = "dev"
+    GithubRepo  = "aws-stacks"
+    GithubOrg   = "letsrockthefuture"
+  }
 }
 
 module "vpc_development" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.12.0"
 
-  name = "vpc-development"
+  name = local.vpc_name
   cidr = "10.0.0.0/16"
 
   azs = [
@@ -22,26 +31,54 @@ module "vpc_development" {
   ]
 
   public_subnets = [
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24",
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24",
   ]
 
   enable_ipv6 = true
 
-  enable_nat_gateway = false
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
-  # public_subnet_tags = {
-  #   Name = "public-development-subnet"
-  # }
-
-  tags = {
-    Owner       = "letsrockthefuture"
-    Environment = "development"
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.eks_name}" = "shared"
+    "kubernetes.io/role/elb"                  = 1
   }
 
-  vpc_tags = {
-    Name = "vpc-development"
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.eks_name}" = "shared"
+    "kubernetes.io/role/internal-elb"         = 1
   }
+
+  tags = local.tags
 }
+
+resource "aws_security_group" "additional" {
+  name_prefix = "${local.eks_name}-additional"
+  vpc_id      = module.vpc_development.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+
+  tags = local.tags
+}
+
+# resource "aws_kms_key" "eks" {
+#   description             = "EKS secret encryption key."
+#   deletion_window_in_days = 7
+#   enable_key_rotation     = true
+
+#   tags = local.tags
+# }
+
